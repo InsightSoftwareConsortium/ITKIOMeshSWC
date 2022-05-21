3831ed22e3e27dc6228aba3fb11602eaa3617385
Matt McCormick <matt.mccormick@kitware.com>
2022-05-21 13:38:58 -0400

    DOC: Improve short description

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=['itk'],
     package_dir={'itk': 'itk'},
     download_url=r'https://github.com/InsightSoftwareConsortium/ITKIOMeshSWC',
-    description=r'ITK module for reading meshes from SWC files, a human- and machine-readable format for representing neuron morphology.',
+    description=r'ITK module for reading meshes from SWC files, a format for representing neuron morphology.',
     long_description='ITK is an open-source, cross-platform library that provides developers with an extensive suite of software tools for image analysis. Developed through extreme programming methodologies, ITK employs leading-edge algorithms for registering and segmenting multidimensional scientific images.',
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
